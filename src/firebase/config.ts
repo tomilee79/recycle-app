ce2d23708f3d3a0e92f6a795ce74749c665c5608
이홍열(AAMOND) <tomilee@dxconsulting.co.kr>
2025-11-16 04:05:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2211023737-8e415",
+  "appId": "1:1050005299720:web:a6f77814c28b2f04abe7bd",
+  "apiKey": "AIzaSyCKfxkxt9AiBPpZ6j8CXFqSRasWOOzp4Rs",
+  "authDomain": "studio-2211023737-8e415.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1050005299720"
+};
