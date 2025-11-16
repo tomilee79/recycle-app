e50fde631c1ecc3ea3847ac02ca90e7b82916270
이홍열(AAMOND) <tomilee@dxconsulting.co.kr>
2025-11-16 09:32:02 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/components/users/users-panel.tsx
+++ src/components/users/users-panel.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import { Card, CardHeader, CardTitle, CardContent, CardDescription, CardFooter } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Badge } from "@/components/ui/badge";
